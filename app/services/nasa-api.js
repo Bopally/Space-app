2950ccc9b51ce1fd9d3381cc6011bca6716bdd39
Estelle Ballot <estelle.ballot@qonto.com>
2025-03-13 09:50:28 +0100

    Improve error handling for API requests

--- app/services/nasa-api.js
+++ app/services/nasa-api.js
@@ -5,8 +5,10 @@ import fetch from 'fetch';
 
 export default class NasaApiService extends Service {
   @tracked apod = null;
+  @tracked isLoading = false;
+  @tracked isError = false;
+  @tracked data = null;
 
-  @action
   async fetchApod(date = null) {
     const apiKey = 'QS3eUr2s8gzKDr19jnh67Gue0SYlcbjklgKewKlh';
     let url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
@@ -16,11 +18,21 @@ export default class NasaApiService extends Service {
     }
 
     try {
+      this.isLoading = true;
+      this.data = null;
+      this.isError = false;
       let response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
       let data = await response.json();
       this.apod = data;
+      this.data = data;
     } catch (error) {
       console.error('Error can not fetch APOD', error);
+      this.isError = true;
+    } finally {
+      this.isLoading = false;
     }
   }
 }
