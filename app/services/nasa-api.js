2e42a58e011378a8c6910678d5ed9f46a8a72c2c
@khaled.khalifa <khaled.khalifa@qonto.com>
2025-03-14 09:12:35 +0100

    Add ENV variable configs

--- app/services/nasa-api.js
+++ app/services/nasa-api.js
@@ -1,7 +1,10 @@
 import Service from '@ember/service';
 import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
 import fetch from 'fetch';
+
+import config from 'nasa-app/config/environment';
+
+const apiKey = config.APP.NASA_API_KEY;
 
 export default class NasaApiService extends Service {
   @tracked apod = null;
@@ -10,7 +13,6 @@ export default class NasaApiService extends Service {
   @tracked data = null;
 
   async fetchApod(date = null) {
-    const apiKey = 'QS3eUr2s8gzKDr19jnh67Gue0SYlcbjklgKewKlh';
     let url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
 
     if (date) {
